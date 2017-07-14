6cec6638f956571070f2c797542784985c1f600e
jakegerard <jake.a.gerard@gmail.com>
2017-07-14 14:16:53 +0000

    Fixes Parallel Paraview Error

--- create.py
+++ create.py
@@ -34,17 +34,13 @@ Cs = float(input_data.readline())
 rho_lbm = float(input_data.readline())
 u_lbm = float(input_data.readline())
 omega = float(input_data.readline())
-
 Nx = int(input_data.readline())
 Ny = int(input_data.readline())
 Nz = int(input_data.readline())
-
 Restart_flag = int(input_data.readline())
-
 Lx_p = float(input_data.readline())
 Ly_p = float(input_data.readline())
 Lz_p = float(input_data.readline())
-
 t_conv_fact = float(input_data.readline())
 l_conv_fact = float(input_data.readline())
 p_conv_fact = float(input_data.readline())
@@ -53,13 +49,11 @@ input_data.close()
 
 u_conv_fact = t_conv_fact/l_conv_fact;
 nnodes = Nx*Ny*Nz
-
 x = np.linspace(0.,Lx_p,Nx).astype(np.float64);
 y = np.linspace(0.,Ly_p,Ny).astype(np.float64);
 z = np.linspace(0.,Lz_p,Nz).astype(np.float64);
 numEl = Nx*Ny*Nz
 Y,Z,X = np.meshgrid(y,z,x);
-
 XX = np.reshape(X,numEl)
 YY = np.reshape(Y,numEl)
 ZZ = np.reshape(Z,numEl)
@@ -67,29 +61,39 @@ ZZ = np.reshape(Z,numEl)
 # compute the number of data dumps I expect to process
 nDumps = (Num_ts-Warmup_ts)/plot_freq + 1 
 
+# Runs each data dump in serial
 for i in range(nDumps):
   rho_fn = 'density'+str(i)+'.b_dat'
   ux_fn = 'ux'+str(i)+'.b_dat'
   uy_fn = 'uy'+str(i)+'.b_dat'
   uz_fn = 'uz'+str(i)+'.b_dat'
 
+  # Create numpy array from the binary data files
   ux = np.fromfile(ux_fn,dtype=np.float32)
   uy = np.fromfile(uy_fn,dtype=np.float32)
   uz = np.fromfile(uz_fn,dtype=np.float32)
   pressure = np.fromfile(rho_fn,dtype=np.float32)
 
-  # convert velocity to physical units
+  # Convert to physical units
   ux /= u_conv_fact
   uy /= u_conv_fact
   uz /= u_conv_fact
   pressure /= p_conv_fact 
-  h5_file = 'out'+str(i)+'.h5'
-  xmf_file = 'data'+str(i)+'.xmf'
+
+  # Create dimensions tuple for pressure reshape and XMF writer
   dims = (Nz,Ny,Nx)
+  
+  # Reshape pressure array
+  # MUST BE DONE TO RUN PARAVIEW IN PARALLEL
+  pressure = pressure.reshape(dims)
 
   if i==0:
     print "Dimensions are",dims
   print "Processing data dump #",i
 
+  # Write output files
+  h5_file = 'out'+str(i)+'.h5'
+  xmf_file = 'data'+str(i)+'.xmf'
   writeH5(pressure,ux,uy,uz,h5_file)
   writeXdmf(dims,xmf_file,i)
+
